c3cdaf1f6a5187a24b505f66daa7b63ac9f6e0fe
Alexodkuz <alexodkuz@gmail.com>
2024-06-17 23:18:17 +0200

    --

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -34,19 +34,25 @@ export default function CheckoutForm() {
 
     setIsProcessing(true);
 
-    const { error, paymentIntent } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        return_url: `${window.location.origin}/completion`,
-        receipt_email: email,
-      },
-    });
+    try {
+      const { error, paymentIntent } = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${window.location.origin}/completion?payment_intent=${paymentIntent?.id}`,
+          receipt_email: email,
+        },
+      });
 
-    if (error) {
-      setMessage(error.type === "card_error" || error.type === "validation_error" ? error.message : "Произошла неожиданная ошибка.");
-      setIsProcessing(false);
-    } else {
-      setMessage(paymentIntent.status === 'succeeded' ? 'Платёж успешно выполнен.' : 'Платёж не выполнен.');
+      if (error) {
+        setMessage(error.type === "card_error" || error.type === "validation_error" ? error.message : "Произошла неожиданная ошибка.");
+        setIsProcessing(false);
+      } else {
+        setMessage(paymentIntent.status === 'succeeded' ? 'Платёж успешно выполнен.' : 'Платёж не выполнен.');
+        setIsProcessing(false);
+      }
+    } catch (e) {
+      console.error('Payment confirmation error:', e);
+      setMessage('Произошла ошибка при подтверждении платежа.');
       setIsProcessing(false);
     }
   };
@@ -56,10 +62,10 @@ export default function CheckoutForm() {
       <div className="info-form">
         <h2>Информация о покупке</h2>
         <div className="event-details small-bold-text">
-          <p>Dortmund</p>
-          <p>Light Club</p>
-          <p>Дата: 14 Июня 2024</p>
-          <p>Цена билета: 10 Euro.</p>
+          <p>Offenbach am Main</p>
+          <p>Scala Club</p>
+          <p>Дата: 28 Июня 2024</p>
+          <p>Цена билета: 12 Euro.</p>
         </div>
         <p>Пожалуйста, введите ваш адрес электронной почты:</p>
         <input type="email" value={email} onChange={handleEmailChange} placeholder="Введите ваш email" />
--- src/Completion.js
+++ src/Completion.js
@@ -1,5 +1,101 @@
-function Completion(props) {
-  return <h1>Thank you! 🎉</h1>;
+import React, { useEffect, useState } from 'react';
+
+function Completion() {
+  const [qrCodePath, setQrCodePath] = useState(null);
+  const [pdfPath, setPdfPath] = useState(null);
+  const [isChecking, setIsChecking] = useState(true);
+  const queryParams = new URLSearchParams(window.location.search);
+  const paymentIntentId = queryParams.get('payment_intent');
+
+  useEffect(() => {
+    const checkTicketStatus = async () => {
+      try {
+        const response = await fetch('/api/ticket-status', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ paymentIntentId })
+        });
+
+        if (!response.ok) {
+          console.log('Error');
+          console.log(response.statusText);
+          throw new Error(`Failed to check ticket status: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        if (data.isActive) {
+          fetchTicketInfo();
+        } else {
+          setTimeout(checkTicketStatus, 2000); // Повторить через 3 секунды
+        }
+      } catch (error) {
+        console.error('Error checking ticket status:', error);
+        setTimeout(checkTicketStatus, 2000); // Повторить через 3 секунды
+      }
+    };
+
+    const fetchTicketInfo = async () => {
+      try {
+        const response = await fetch('/api/ticket-info', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ paymentIntentId })
+        });
+
+        if (!response.ok) {
+          console.log('Error');
+          console.log(response.statusText);
+          throw new Error(`Failed to fetch ticket info: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setQrCodePath(data.qrCodePath);
+        setPdfPath(`${window.location.origin}/tickets/${data.pdfPath.split('/').pop()}`);
+        setIsChecking(false);
+      } catch (error) {
+        console.error('Error fetching ticket info:', error);
+        setIsChecking(false);
+      }
+    };
+
+    if (paymentIntentId) {
+      checkTicketStatus();
+    }
+  }, [paymentIntentId]);
+
+  return (
+    <div style={{
+      fontFamily: 'Arial, sans-serif',
+      color: '#333',
+      textAlign: 'center',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '50px'
+    }}>
+      <h1>Спасибо! Оплата прошла успешно!</h1>
+      <p>Ваш билет был отправлен на указанный вами E-Mail</p>
+      {isChecking && <p>Подождите, ваш билет создается...</p>}
+      {qrCodePath && (
+        <div>
+          <h2>Ваш QR-код</h2>
+          <img src={qrCodePath} alt="QR Code" style={{ maxWidth: '200px' }} />
+        </div>
+      )}
+      {pdfPath && (
+        <div>
+          <a href={pdfPath} download="Ваш_билет.pdf" style={{ marginTop: '20px', display: 'block', textDecoration: 'none', backgroundColor: '#FF5722', color: '#fff', padding: '10px 20px', borderRadius: '5px' }}>Скачать билет (PDF)</a>
+        </div>
+      )}
+      <p>Если у вас возникли проблемы, напишите нам: <a href="https://www.instagram.com/tusa_koeln" target="_blank" rel="noopener noreferrer">instagram @tusa_koeln</a></p>
+    </div>
+  );
 }
 
 export default Completion;
